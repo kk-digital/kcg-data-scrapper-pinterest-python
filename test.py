0e3802bdf973960404b6e789db7736327895513d
Omar Mohamed <metwallyo123@gmail.com>
2022-12-30 08:29:47 +0200

    some changes to example loop code

--- test.py
+++ test.py
@@ -3,11 +3,14 @@ import time
 from helper_functions import init_driver
 from bs4 import BeautifulSoup
 import re
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # constants
 SCROLL_IDLE_TIME = 0.5
-POLL_TIME = 0.5
+POLL_TIME = 1
 SCROLL_TIMEOUT = 10
+TRIGGER_STOP = 5 # If your internet connection is slow, increase this number.
 
 # Stage 1 class 
 class Stage1:
@@ -57,24 +60,31 @@ class Stage1:
     def __get_boards(self, html):
         
         soup = BeautifulSoup(html, 'html.parser')
+
+        div_list = soup.find_all("div")
+        list_item_counter = 0 
+        for div in div_list:
+            try:
+                if div["role"] == "listitem":
+                    #print("list item found")
+                    list_item_counter += 1 
+            except:
+                continue
+        print(f"number of list items = {list_item_counter}")
         links_list = soup.find_all("a")
 
         for a in links_list:
             url = a["href"]
-            print(url)
             image_count = self.__get_image_count(a)
             board_name =  self.__get_board_name(a)
             self.all_data.append({'url':url,'image_count':image_count,'board_name':board_name})
         
-        for url in self.all_data:
-            print(url)
         print(f"scrapped links:  {len(links_list)}")
         return len(links_list)
 
-    def scrape_boards_urls(self):
-        self.driver.maximize_window()
-        #self.driver.set_window_size(1280,2500) # w, h
+    def scrape_boards_urls(self, height):
         tag_div = self.driver.find_element(By.XPATH , "//div[@class='gridCentered']")
+        #tag_div = self.driver.find_element(By.TAG_NAME, "html")
         return self.__get_boards(tag_div.get_attribute('innerHTML'))
 
 
@@ -83,8 +93,9 @@ def get_scroll_height(driver):
 
 def scroll_down(driver):
     scroll_height = get_scroll_height(driver) 
-    driver.execute_script(f"window.scrollTo(0, {scroll_height});")
-    
+    driver.execute_script(f"window.scrollBy(0,{scroll_height});")
+    #driver.execute_script("window.scrollBy(0, Math.abs(window.innerHeight-5) );")
+
     
 def get_page_hash(driver):
     dom = driver.find_element(By.TAG_NAME,'html').get_attribute('innerHTML')
@@ -92,8 +103,10 @@ def get_page_hash(driver):
 
 
 def main(search_term):
-    driver = init_driver() 
+    driver = init_driver()
     driver.delete_all_cookies()
+    driver.execute_script("document.body.style.zoom='50%'")
+    driver.maximize_window()
     search_term = search_term.replace(" ", "%20")
     driver.get(f"https://www.pinterest.com/search/boards/?q={search_term}&rs=filter")
     driver.set_script_timeout(SCROLL_TIMEOUT)
@@ -112,14 +125,14 @@ def main(search_term):
             time.sleep(SCROLL_IDLE_TIME)
             scroll_down(driver=driver)
             time.sleep(SCROLL_IDLE_TIME)
-        
+            
             time.sleep(POLL_TIME) # wait 0.5 seconds and check 
 
         except TimeoutError:
             print("[WARNING] TimeOut !")
             timeout_counter += 1
             continue
-        
+
         new_height   = get_scroll_height(driver)
         page_hash_new = get_page_hash(driver)
 
@@ -127,17 +140,19 @@ def main(search_term):
 
         if new_height == old_height:
             print("[INFO] SAME HEIGHT")
-            trigger_counter += 1 
+            trigger_counter += 1
+        else:
+            trigger_counter = 0  
 
-        if trigger_counter == 6 :
-            print("[INFO] ALL PAGE LOADED")
-            board_url_stage.scrape_boards_urls()
-            break
-        
         if page_hash_new == page_hash: 
             print("[WARNING] SAME HASH - SAME PAGE")
         else:
             print("[INFO] NEW PAGE LOADED")
+
+        if trigger_counter == TRIGGER_STOP and page_hash_new == page_hash:
+            print("[INFO] ALL PAGE LOADED")
+            board_url_stage.scrape_boards_urls(new_height)
+            break
         
 
 
