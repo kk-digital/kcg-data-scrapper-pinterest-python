b9cd4608ddd1f411927df3eeb781872ea267aadf
Omar Mohamed <metwallyo123@gmail.com>
2023-02-14 03:29:55 +0200

    updating stage 1 and stage2 algorithms

--- stage1_board_search.py
+++ stage1_board_search.py
@@ -6,6 +6,16 @@ from selenium.webdriver.common.by import By
 from helper_functions import init_driver , create_database, page_has_loaded
 import time 
 import re
+import socket
+
+SCROLL_IDLE_TIME = 3
+POLL_TIME = 1
+SCROLL_TIMEOUT = 10
+TRIGGER_STOP = 3 # If your internet connection is slow, increase this number.
+RESIZE_WAIT_TIME = 10
+TIMEOUT = 10
+
+
 
 class Stage1:
     """ Stage1: getting all boards urls for a given search term"""
@@ -15,6 +25,7 @@ class Stage1:
         self.all_data = {}
         self.driver = None
         self.db_conn = None
+        
 
     def __start_connections(self):
         self.db_conn  = self.__initiate_db_conn() 
@@ -90,43 +101,21 @@ class Stage1:
 
         for a in links_list:
             url = a["href"]
-            #print(url)
             image_count = self.__get_image_count(a)
             board_name =  self.__get_board_name(a)
             sections_count = self.__get_sections_count(a)
             self.all_data[url] = [self.search_term, image_count, sections_count,board_name]
         
-        return len(links_list)
     
     def __scrape_boards_urls(self):
-        self.driver.set_window_size(1280, 720)
+        # self.driver.set_window_size(1280, 720)
         tag_div = self.driver.find_element(By.XPATH , "//div[@role='list']")
         return self.__get_boards(tag_div.get_attribute('innerHTML'))
 
-    def get_board_search_result(self):        
-        # make driver settings ready for scrapping.
-        self.driver.delete_all_cookies()
-        self.driver.get("https://www.pinterest.com/search/boards/?q="+self.search_term.replace(" ", "%20")+"&rs=filter")
-        self.driver.execute_script("document.body.style.zoom='50%'")
-        #self.driver.maximize_window()
 
-        # scrape boards url
-        url_count = self.__scrape_boards_urls()
-        # scroll 
-        self.driver.execute_script("window.scrollBy(0, Math.abs(window.innerHeight-5) );")
-        page_has_loaded(driver=self.driver)
-        
-        
-        same_url_count = 0
-        while same_url_count < 5 :
-            new_url_count = self.__scrape_boards_urls()
-            
-            if new_url_count == url_count:
-                same_url_count += 1
+    def __get_scroll_height(self):
+        return self.driver.execute_script("return document.documentElement.scrollHeight")
 
-            url_count = new_url_count
-            self.driver.execute_script("window.scrollBy(0, Math.abs(window.innerHeight-5) );")
-            page_has_loaded(driver=self.driver)
 
     def __check_existance(self, board_url):
         """checking if a board url + search term is in DB or not"""
@@ -176,19 +165,111 @@ class Stage1:
     def __exit_stage(self):
         self.db_conn.close()
         self.driver.quit()
-            
+
+    def __get_scroll_height(self):
+        return self.driver.execute_script("return document.documentElement.scrollHeight")
+
+
+    def __scroll_inner_height(self):
+        self.driver.execute_script("window.scrollBy(0, Math.abs(window.innerHeight * 0.8));")
+    
+    def __is_netwrok_available(self):
+        try:
+            # Connect to a known website to check if there is an active network connection
+            socket.create_connection(("www.pinterest.com", 80))
+            return True
+        except OSError:
+            pass
+        return False
+
+
+
+    def __scroll_and_scrape(self):
+        try:
+            print(f"[INFO] STARTING SCROLLING AND SCRAPPING")
+            self.driver.delete_all_cookies()
+            self.driver.execute_script("document.body.style.zoom='50%'")
+            self.driver.get("https://www.pinterest.com/search/boards/?q="+self.search_term.replace(" ", "%20")+"&rs=filter")
+
+            scroll_trigger_count = 0
+            pins_trigger_count = 0 
+            while scroll_trigger_count <= 10 and pins_trigger_count <= TRIGGER_STOP:
+
+                before_pins_count = len(self.all_data)
+                before_scroll_height = self.__get_scroll_height()
+
+                print(f"[INFO] SCROLLING")
+                self.__scroll_inner_height()
+                time.sleep(SCROLL_IDLE_TIME)
+                # Scrapping the pins.
+                try:
+                    self.__scrape_boards_urls()
+                except Exception as e:
+                    print(f"[ERROR] IN GETTING URLS; {e}")
+                    pins_trigger_count = 0 
+                    scroll_trigger_count = 0
+
+                    if not self.__is_netwrok_available():
+                        print("[ERROR] NETWORK ERROR; PLEASE CHECK")
+                        print("[INFO] RESTARTING THE SCROLL AND SCRAPPING FUNCTION")
+                        self.__scroll_and_scrape()
+                    
+                    continue
+
+                print(f"[INFO] NUMBER OF URLS SCRAPPED: {len(self.all_data)}")
+                
+                print(f"[INFO] SCROLLING")           
+                self.__scroll_inner_height()
+                time.sleep(SCROLL_IDLE_TIME)
+                # Scrapping the pins.
+                try:
+                    self.__scrape_boards_urls()
+                except Exception as e:
+                    print(f"[ERROR] IN GETTING URLS; {e}")
+                    pins_trigger_count = 0 
+                    scroll_trigger_count = 0
+
+                    if not self.__is_netwrok_available():
+                        print("[ERROR] NETWORK ERROR; PLEASE CHECK")
+                        print("[INFO] RESTARTING THE SCROLL AND SCRAPPING FUNCTION")
+                        self.__scroll_and_scrape()     
+                    continue
+
+                print(f"[INFO] NUMBER OF URLS SCRAPPED: {len(self.all_data)}")                
+                after_pins_count = len(self.all_data)
+                after_scroll_height = self.__get_scroll_height()
+
+                # Check if the scroll height is the same
+                if before_scroll_height == after_scroll_height:
+                    scroll_trigger_count += 1
+                    print("[INFO] SCROLL STOP TRIGGER COUNT INCREASED")
+                else :
+                    scroll_trigger_count = 0
+                    print("[INFO] SCROLL STOP TRIGGER IS ZERO NOW")
+
+                if after_pins_count == before_pins_count:
+                    pins_trigger_count += 1
+                    print("[INFO] PINS COUNT STOP TRIGGER COUNT INCREASED")
+                else :
+                    pins_trigger_count = 0
+                    print("[INFO] PINS COUNT STOP TRIGGER IS ZERO NOW")
+        except Exception as e:
+            print(f"[ERROR] IN SCROLL AND SCRAPE {str(e)}")
+            print("[INFO] WAITING FOR 5 MINUTES")
+            time.sleep(5*60)
+            self.__scroll_and_scrape()
+
+
     def run(self):
         """main function of the program"""   
         self.__start_connections()     
-        self.get_board_search_result()
+        self.__scroll_and_scrape()
+        self.driver.close()
 
         print(f"[INFO] NUMBER OF BOARDS SCRAPPED : {len(self.all_data)}")
         for url in self.all_data:
+            print(url)
             self.__push_into_db(str(url), int(self.all_data[url][1].strip().replace(",","")),self.all_data[url][2])
 
         self.__exit_stage()
-
-
- 
-
 
--- stage2_board_url_scraping.py
+++ stage2_board_url_scraping.py
@@ -11,7 +11,8 @@ from selenium import webdriver
 import time
 import re
 import os 
-from helper_functions import page_has_loaded , init_driver
+from helper_functions import page_has_loaded, init_driver, create_database
+import socket
 
 
 # creating output folder.
@@ -23,6 +24,14 @@ Separator_for_csv = "\t"
 DATABASE_PATH = "database.db"
 HOW_MANY_WINDOWS_DO_YOU_NEED = 1
 true_pin_count = None # true number of pins for a single board
+SCROLL_IDLE_TIME = 3
+POLL_TIME = 1
+SCROLL_TIMEOUT = 10
+TRIGGER_STOP = 5 # If your internet connection is slow, increase this number.
+RESIZE_WAIT_TIME = 10
+TIMEOUT = 10
+THREAD_POOL_WORKERS = 5 # Size of the thread pool
+
 
 
 class window:
@@ -83,7 +92,6 @@ class window:
         get_url = self.driver.current_url
         #print(f"[INFO] THE CURRENT PAGE WINDOW IN IS {get_url}")
         self.driver.implicitly_wait(20) # gives an implicit wait for 20 seconds
-        
         
         try: 
             mother_of_a_tag = self.driver.find_element(By.XPATH, "//div[@class='gridCentered']")                
@@ -159,35 +167,6 @@ def get_true_pins_count(board):
     return true_pins_count
 
 
-def process(board_list):
-    driver = init_driver()
-    driver.set_window_size(1280,720)
-    windows = [window(driver, i) for i in driver.window_handles]
-    
-    for index in range(len(board_list)):
-        board = board_list[index]
-        true_pin_count =  get_true_pins_count(board)
-        board_url = f"https://www.pinterest.com{board}"
-        windows[0].load_board_page(board_url)
-        windows[0].get_link_pin()
-        print(f"[INFO] STARTING PINS SCRAPING FOR BOARD {board}")
-
-        first_roll = True
-        try:
-            while(windows[0].is_loaded_full_images(first_roll)): # waiting for the page to scroll and then collect pins
-                try:
-                    windows[0].get_link_pin()
-                    links_count = len(windows[0].all_links)
-                    print(f"[INFO] {links_count} pins scrapped out of {true_pin_count} in board {board_url}")
-                    first_roll = False
-                    windows[0].is_loaded_full_images(first_roll)
-                except Exception as e:
-                    print(f"[ERROR] IN PAGE LOOP, {e}")
-        except Exception as e:
-            print("[ERROR] BEFORE GOING TO THE PAGE LOOP, {e}")
-            index -= 1
-        #set_board_is_scraped(board_url)
-        print(f"[INFO] FINISHED URL:{board_url}; PINS = {links_count}; TRUE NUMBER = {true_pin_count}")
 
 def count_pins_in_board(board_url):
     pins_count = None
@@ -229,107 +208,275 @@ def get_true_pins_count(board):
     return true_pins_count
 
 
+def process(board_list):
+    driver = init_driver()
+    driver.set_window_size(1280,720)
+    windows = [window(driver, i) for i in driver.window_handles]
+    
+    for index in range(len(board_list)):
+        board = board_list[index]
+        true_pin_count =  get_true_pins_count(board)
+        board_url = f"https://www.pinterest.com{board}"
+        windows[0].load_board_page(board_url)
+        windows[0].get_link_pin()
+        print(f"[INFO] STARTING PINS SCRAPING FOR BOARD {board}")
 
-def get_board_urls(search_term):
-    returns = []
-    #cmd = "select board_url from stage1"
-    try:
-        with sqlite3.connect(DATABASE_PATH) as conn:
-            cursor = conn.execute("SELECT board_url from stage1 WHERE search_term=?",(search_term,))
-            conn.commit()
-            for i in cursor:
-                returns.append(i[0])
+        first_roll = True
+        try:
+            while(windows[0].is_loaded_full_images(first_roll)): # waiting for the page to scroll and then collect pins
+                try:
+                    windows[0].get_link_pin()
+                    links_count = len(windows[0].all_links)
+                    print(f"[INFO] {links_count} pins scrapped out of {true_pin_count} in board {board_url}")
+                    first_roll = False
+                    windows[0].is_loaded_full_images(first_roll)
+                except Exception as e:
+                    print(f"[ERROR] IN PAGE LOOP, {e}")
+        except Exception as e:
+            print("[ERROR] BEFORE GOING TO THE PAGE LOOP, {e}")
+            index -= 1
+        #set_board_is_scraped(board_url)
+        print(f"[INFO] FINISHED URL:{board_url}; PINS = {links_count}; TRUE NUMBER = {true_pin_count}")
 
-    except Exception as e :
-        print(f"[ERROR] cannot get the boards urls!, because of {e}")
-        time.sleep(1)
-        return get_board_urls(search_term)
-    return returns
-
-# def get_search_term():
-#     cmd = "select search_term from stage1 LIMIT 1;"
-#     try:
-#         with sqlite3.connect(DATABASE_PATH) as conn:
-#             cursor = conn.execute(cmd)
-#             conn.commit()
-#             for i in cursor:
-#                 return i[0]
-#     except:
-#         time.sleep(1)
-#         return get_search_term()
 
 
 
 class Stage2: 
-    def __init__(self,search_term) -> None:
+    def __init__(self,search_term):
         self.search_term = search_term
-    
-    def run(self) -> None: 
-        """
-            function that executes the second stage of Pintrest scraping, which is retrieveing the board urls which was collected 
-            in stage 1 and goes in them 1 by one to collect all pens urls within those boards and storing the pin urls inside the sqlite DB. 
-        """
+        self.db_conn = None
+        self.all_links = []
+
+    def __start_connections(self):
+        self.db_conn  = self.__initiate_db_conn() 
+        self.driver = init_driver()
+
+
+    def __initiate_db_conn(self):
+        """ getting the connection object to the DB""" 
+        if not os.path.exists(DATABASE_PATH):
+            create_database()
+        return sqlite3.connect(DATABASE_PATH)
         
-        board_urls = get_board_urls(self.search_term)
+
+    def __count_pins_in_board(self):
+        pins_count = None
+        try:
+            with sqlite3.connect(DATABASE_PATH) as conn:
+                cursor = conn.execute("SELECT count(*) as pins_count from stage2 WHERE board_url=?",(self.board_url,)).fetchall()
+                conn.commit()
+                pins_count = cursor[0][0]
+
+        except Exception as e :
+            print(f"[ERROR] cannot count pins in stage2 in board {self.board_url}!, because of {e}")
+            time.sleep(1)
+            return self.__count_pins_in_board(self.board_url)
+        return pins_count
+
+
+    def __exist_in_db(self, pin_url):
+        """checking if a board url + pin_url is in DB or not"""
+        exist = 0
+        try:
+            with sqlite3.connect(DATABASE_PATH) as conn:
+                cursor = conn.execute("SELECT count(*) FROM stage2 WHERE board_url=? AND pin_url=?",(self.board_url,pin_url)).fetchone()
+                conn.commit()
+                exist = cursor[0]
+
+        except Exception as e :
+            print(f"[ERROR] cannot check board existance , because of {e}")
+            time.sleep(1)
+            return self.__exist_in_db(pin_url)
+        return exist
+
+    def __push_to_database(self, pin_url):
+        if self.__exist_in_db(pin_url):
+            #print(f"[INFO] {pin_url} with {self.board_url} exists.")
+            return 
+        else:
+            #print(f"[INFO] inserting {pin_url} with {self.board_url}.")
+            self.__insert_to_database(pin_url)
+
+    def __insert_to_database(self, pin_url):
+        cmd = "insert into stage2(board_url, pin_url) values ('" + \
+            str(self.board_url)+"','"+str(pin_url)+"')"
+        try:
+            with sqlite3.connect(DATABASE_PATH) as conn:
+                conn.execute(cmd)
+                conn.commit()
+                print(f"[INFO] INSERTING {pin_url}")
+        except Exception as e:
+            if(str(e).lower().find("unique") != -1):
+                pass
+            elif(str(e).lower().find("database is locked") != -1):
+                time.sleep(1)
+                self.__push_to_database(pin_url)
+            else:
+                print(str(e))
+
+
+    def __get_board_urls(self):
+        returns = []
+        try:
+            with sqlite3.connect(DATABASE_PATH) as conn:
+                cursor = conn.execute("SELECT board_url from stage1 WHERE search_term=?",(self.search_term,))
+                conn.commit()
+                returns.extend(url[0] for url in cursor)
+        except Exception as e :
+            print(f"[ERROR] cannot get the boards urls!, because of {e}")
+            time.sleep(1)
+            return self.__get_board_urls()
+        return returns
+
+
+    def __get_link_pin(self):
+        self.driver.implicitly_wait(20)
+        mother_of_a_tag = self.driver.find_element(By.XPATH, "//div[@class='gridCentered']")                
+        mother_of_a_tag = mother_of_a_tag.get_attribute('innerHTML')
+        soup = BeautifulSoup(mother_of_a_tag, 'html.parser')
+        for i in soup.find_all('a'):
+            try:
+                id = i['href']
+            except:
+                continue
+            if(id.find("/pin/") != -1):
+                if id not in self.all_links:
+                    self.all_links.append(id)
+                    self.__push_to_database("https://www.pinterest.com"+id)
+
+
     
-        process(board_urls)
+    def __get_scroll_height(self):
+        return self.driver.execute_script("return document.documentElement.scrollHeight")
 
-        board_pins_count_report(self.search_term)
 
-        #output_json_file()
- 
+    def __scroll_inner_height(self):
+        self.driver.execute_script("window.scrollBy(0, Math.abs(window.innerHeight * 0.8));")
+    
+    def __is_netwrok_available(self):
+        try:
+            # Connect to a known website to check if there is an active network connection
+            socket.create_connection(("www.pinterest.com", 80))
+            return True
+        except OSError:
+            pass
+        return False
+
+
+
+    def __scroll_and_scrape(self, board):
+        try:
+            print(f"[INFO] STARTING SCROLLING AND SCRAPPING FOR BOARD: {board}")
+            self.all_links = []
+            self.driver.delete_all_cookies()
+            self.driver.execute_script("document.body.style.zoom='50%'")
+            self.driver.get(self.board_url)
+            target_number_of_pins = self.__get_true_pins_count(board)
+
+            scroll_trigger_count = 0
+            pins_trigger_count = 0 
+            while scroll_trigger_count <= TRIGGER_STOP and pins_trigger_count <= TRIGGER_STOP:
+
+                before_pins_count = len(self.all_links)
+                before_scroll_height = self.__get_scroll_height()
+
+                print(f"[INFO] {board} SCROLLING")
+                self.__scroll_inner_height()
+                time.sleep(SCROLL_IDLE_TIME)
+                # Scrapping the pins.
+                try:
+                    self.__get_link_pin()
+                except Exception as e:
+                    print(f"[ERROR] IN GETTING PINS; {e}")
+                    pins_trigger_count = 0 
+                    scroll_trigger_count = 0
+
+                    if not self.__is_netwrok_available():
+                        print("[ERROR] NETWORK ERROR; PLEASE CHECK")
+                        print("[INFO] RESTARTING THE SCROLL AND SCRAPPING FUNCTION")
+                        self.__scroll_and_scrape(board)
+                    
+                    continue
+
+                print(f"[INFO]{board} :: NUMBER OF PINS SCRAPPED: {len(self.all_links)} PINS OUT OF {target_number_of_pins}")
+                
+                print(f"[INFO]{board} :: SCROLLING")           
+                self.__scroll_inner_height()
+                time.sleep(SCROLL_IDLE_TIME)
+                # Scrapping the pins.
+                try:
+                    self.__get_link_pin()
+                except Exception as e:
+                    print(f"[ERROR] IN GETTING PINS; {e}")
+                    pins_trigger_count = 0 
+                    scroll_trigger_count = 0
+
+                    if not self.__is_netwrok_available():
+                        print("[ERROR] NETWORK ERROR; PLEASE CHECK")
+                        print("[INFO] RESTARTING THE SCROLL AND SCRAPPING FUNCTION")
+                        self.__scroll_and_scrape(board)
+                                        
+                    continue
+                print(f"[INFO]{board} :: NUMBER OF PINS SCRAPPED: {len(self.all_links)} PINS OUT OF {target_number_of_pins}")                
+                after_pins_count = len(self.all_links)
+
+                print(f"[INFO] {board} NUMBER OF PINS SCRAPPED: {after_pins_count} PINS OUT OF {target_number_of_pins}")            
+                after_scroll_height = self.__get_scroll_height()
+
+                # Check if the scroll height is the same
+                if before_scroll_height == after_scroll_height:
+                    scroll_trigger_count += 1
+                    print(f"[INFO] {board} SCROLL STOP TRIGGER COUNT INCREASED")
+                else :
+                    scroll_trigger_count = 0
+                    print(f"[INFO] {board} SCROLL STOP TRIGGER IS ZERO NOW")
+
+                if after_pins_count == before_pins_count:
+                    pins_trigger_count += 1
+                    print(f"[INFO] {board} PINS COUNT STOP TRIGGER COUNT INCREASED")
+                else :
+                    pins_trigger_count = 0
+                    print(f"[INFO] {board} PINS COUNT STOP TRIGGER IS ZERO NOW")
+        except Exception as e:
+            print(f"[ERROR] IN SCROLL AND SCRAPE {str(e)}")
+            print("[INFO] WAITING FOR 5 MINUTES")
+            time.sleep(5*60)
+            self.__scroll_and_scrape(board)
+
+    def __get_true_pins_count(self, board):
+        true_pins_count = None
+        try:
+            with sqlite3.connect(DATABASE_PATH) as conn:
+                cursor = conn.execute("SELECT pin_count FROM stage1 WHERE board_url=?",(board,)).fetchone()
+                conn.commit()
+                true_pins_count = cursor[0]
+        except Exception as e :
+            print(f"[ERROR] cannot get true pins count!, because of {e}")
+            time.sleep(1)
+            return self.__get_true_pins_count(board)
+        return true_pins_count
+
+
+    def run(self):
+        board_list = self.__get_board_urls()
+        if not board_list:
+            print("[ERROR] THIS SEARCH TERM HAS NO BOARDS IN DB")
+            return 
+        # Starting DB connections and driver.
+        self.__start_connections()
+        for board in board_list:
+            self.board_url = f"https://www.pinterest.com{board}"
+            print(f"[INFO] IN BOARD: {board}")
+            # Check if board already scrapped
+            if 0.95*self.__get_true_pins_count(board) <= self.__count_pins_in_board():
+                print(f"[INFO] BOARD {board} ALREADY SCRAPPED")
+                continue
+            
+            self.__scroll_and_scrape(board)
+            print(f"[INFO] BOARD {board} ; SCRAPPED {len(self.all_links)} ; TARGET {self.__get_true_pins_count(board)}")
+
+        self.driver.close()
+            
         return 
 
 
-# def set_board_is_scraped(url):
-#     cmd = "update stage1 set scraped = 1 where board_url = '"+url+"';"
-#     try:
-#         with sqlite3.connect(DATABASE_PATH) as conn:
-#             conn.execute(cmd)
-#             conn.commit()
-#     except Exception as e:
-#         print(str(e))
-#         time.sleep(1)
-#         return set_board_is_scraped(url)
-    
-        
-# def output_json_file():
-#     json_data = []
-#     data = {}
-#     number_of_images = {}
-#     cmd = "select board_url, pin_url from stage2"
-#     with sqlite3.connect(DATABASE_PATH) as conn:
-#         cursor = conn.execute(cmd)
-#         conn.commit()
-#     cursor_temp = []
-#     for i in cursor:
-#         cursor_temp.append(i)
-#     cmd = "select board_url, pin_url, count(pin_url) from stage2 group by board_url"
-#     with sqlite3.connect(DATABASE_PATH) as conn:
-#         cursor_for_pins = conn.execute(cmd)
-#         conn.commit()
-#     for cur in cursor_for_pins:
-#         number_of_images[cur[0]] = cur[2]
-#     for cur in cursor_temp:
-#         data[cur[0]] = None
-    
-#     count = 0
-#     for board_url in data:
-#         count+=1
-#         print("Writting ", count," -> ", board_url)
-#         pins = []
-#         for cur in cursor_temp:
-#             if(cur[0] == board_url):
-#                 pins.append(cur[1])
-#         # print({"board url": board_url, "number of images": number_of_images[board_url], "pins": pins})
-#         json_data.append({"board url": board_url, "number of images": number_of_images[board_url], "pins": pins})
-#     json_data = {get_search_term():json_data}
-#     json_string = json.dumps(json_data)
-
-#     with open(file_out_path, 'w') as outfile:
-#         outfile.write(json_string)
-
-# if __name__ == '__main__':
-#     stage2 = Stage2()
-#     stage2.run()
-    
